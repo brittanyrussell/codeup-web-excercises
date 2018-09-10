a2f6e517fcec036fa37f68f2b812193b3a03ebbf
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-10 15:49:43 -0500

    update

---
+++ js/promises.js
@@ -0,0 +1,9 @@
+function wait(num) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+         resolve();
+        }, num);
+    });
+}
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
