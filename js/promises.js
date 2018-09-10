9653598804fc6e8d80ba9b4d5967bb7864b03b3d
brittanyrussell <brittanyrussell11032gmail.com>
2018-09-10 16:57:47 -0500

    finished promises exercise

--- js/promises.js
+++ js/promises.js
@@ -7,3 +7,14 @@ function wait(num) {
 }
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+let url = "https://api.github.com/repos/brittanyrussell/codeup-web-exercises/events";
+
+fetch(url).then(response => {
+    response.json().then(commits => {
+        console.log(commits[0]);
+    });
+});
+
+fetch(url, {headers: {'Authorization': 'bb87a281431739091f6f05a197f6576a093f536e'}});
